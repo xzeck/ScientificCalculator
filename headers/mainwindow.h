05a51e13c533e07bcf12147c84ca49f926274f59
ajay <ajaypnair99@gmail.com>
2019-04-04 00:50:08 +0530

    Added keyboard handling

--- headers/mainwindow.h
+++ headers/mainwindow.h
@@ -19,6 +19,7 @@ public:
 private:
   Ui::MainWindow *ui;
   QString HistoryString = "";
+  void keyPressEvent(QKeyEvent *event);
 
 private slots:
   void NumPressed();
--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -12,8 +12,7 @@
 #include <QUrl>
 #include <math.h>
 #include <QString>
-
-
+#include <QKeyEvent>
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -21,10 +20,6 @@ MainWindow::MainWindow(QWidget *parent) :
   ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
-  //TODO : Add CSS to the form
-  //TODO : Add Github link
-  //TODO : Add a link to view license
-  //TODO : Add Quit functionality
 
   QPushButton *NumButtons[10];
   QPushButton *MathButton;
@@ -111,86 +106,87 @@ void MainWindow::on_btDel_clicked()
   QString Display = ui->Display->text();
   std::vector<QString> NewDisplayVec;
 
-  //FIXME : Delete button causes underflow and crash when there's no text in Display
-
-  for(auto x : Display)
+  if(Display.length() != 0)
     {
-      NewDisplayVec.emplace_back(x);
-    }
+      for(auto x : Display)
+        {
+          NewDisplayVec.emplace_back(x);
+        }
 
-  Display = "";
-  for(unsigned long i =0; i<NewDisplayVec.size()-1; i++)
-    {
-      Display += NewDisplayVec[i];
-    }
+      Display = "";
+      for(unsigned long i =0; i<NewDisplayVec.size()-1; i++)
+        {
+          Display += NewDisplayVec[i];
+        }
 
-  ui->Display->setText(Display);
+      ui->Display->setText(Display);
+    }
 }
 
 void MainWindow::on_btClr_clicked()
 {
-    ui->Display->clear();
+  ui->Display->clear();
 }
 
 void MainWindow::on_actionAbout_triggered()
 {
   QMessageBox Message;
-    QRect  Geo = MainWindow::geometry();
-    int MBoxWidth = Geo.width()/4;
-    int MBoxHeight = Geo.height()/4;
+  QRect  Geo = MainWindow::geometry();
+  int MBoxWidth = Geo.width()/4;
+  int MBoxHeight = Geo.height()/4;
 
-    int X = Geo.x() + Geo.width()/2 - (MBoxWidth/2);
-    int Y = Geo.y() + Geo.height()/2 - (MBoxHeight/2);
-    Message.setGeometry(X,Y,MBoxWidth, MBoxHeight);
-    Message.setInformativeText("Project : Scientific Calculator\n\nAjay Nair : 402\n"\
-                               "Gladson Daniel Roy : 413\nHrishikesh Nair : 418");
-    Message.setIcon(Message.Information);
-    Message.button(Message.Ok);
+  int X = Geo.x() + Geo.width()/2 - (MBoxWidth/2);
+  int Y = Geo.y() + Geo.height()/2 - (MBoxHeight/2);
+  Message.setGeometry(X,Y,MBoxWidth, MBoxHeight);
+  Message.setInformativeText("Project : Scientific Calculator\n\nAjay Nair : 402\n"\
+                             "Gladson Daniel Roy : 413\nHrishikesh Nair : 418");
+  Message.setIcon(Message.Information);
+  Message.button(Message.Ok);
   Message.exec();
 }
 
 void MainWindow::on_actionAjay_Nair_triggered()
 {
   if(!QDesktopServices::openUrl(QUrl("https://github.com/xzeck/",QUrl::TolerantMode)))
-     {
-       QMessageBox Message;
-       QRect  Geo = MainWindow::geometry();
-       int MBoxWidth = Geo.width()/4;
-       int MBoxHeight = Geo.height()/4;
+    {
+      QMessageBox Message;
+      QRect  Geo = MainWindow::geometry();
+      int MBoxWidth = Geo.width()/4;
+      int MBoxHeight = Geo.height()/4;
 
-       int X = Geo.x() + Geo.width()/2 - (MBoxWidth/2);
-       int Y = Geo.y() + Geo.height()/2 - (MBoxHeight/2);
-       Message.setGeometry(X,Y,MBoxWidth, MBoxHeight);
-       Message.setInformativeText("Sorry the URL cannot be opened at this instance because a browswer"\
-                                  "cannot be found.\nYou can view the profile at \nhttps://github.com/xzeck/");
+      int X = Geo.x() + Geo.width()/2 - (MBoxWidth/2);
+      int Y = Geo.y() + Geo.height()/2 - (MBoxHeight/2);
+      Message.setGeometry(X,Y,MBoxWidth, MBoxHeight);
+      Message.setInformativeText("Sorry the URL cannot be opened at this instance because a browswer"\
+                                 "cannot be found.\nYou can view the profile at \nhttps://github.com/xzeck/");
 
-       Message.setWindowTitle("cannot open browser");
-       Message.setIcon(Message.Information);
-       Message.button(Message.Ok);
-       Message.exec();
- }
+      Message.setWindowTitle("cannot open browser");
+      Message.setIcon(Message.Information);
+      Message.button(Message.Ok);
+      Message.exec();
+    }
 }
 
 void MainWindow::on_actionHrishikesh_Nair_triggered()
 {
   if(!QDesktopServices::openUrl(QUrl("https://github.com/DarkStar24",QUrl::TolerantMode)))
-      {
-        QMessageBox Message;
-        QRect  Geo = MainWindow::geometry();
-        int MBoxWidth = Geo.width()/4;
-        int MBoxHeight = Geo.height()/4;
+    {
+      QMessageBox Message;
+      QRect  Geo = MainWindow::geometry();
+      int MBoxWidth = Geo.width()/4;
+      int MBoxHeight = Geo.height()/4;
 
-        int X = Geo.x() +  Geo.width()/2 - (MBoxWidth/2);
-        int Y = Geo.y() + Geo.height()/2 - (MBoxHeight/2);
-        Message.setGeometry(X,Y,MBoxWidth, MBoxHeight);
-        Message.setInformativeText("Sorry the URL cannot be opened at this instance because a browswer"\
-                                   "cannot be found.\nYou can view the profile at \nhttps://github.com/DarkStar24");
+      int X = Geo.x() +  Geo.width()/2 - (MBoxWidth/2);
+      int Y = Geo.y() + Geo.height()/2 - (MBoxHeight/2);
+      Message.setGeometry(X,Y,MBoxWidth, MBoxHeight);
+      Message.setInformativeText("Sorry the URL cannot be opened at this instance because a browswer"\
+                                 "cannot be found.\nYou can view the profile at \nhttps://github.com/DarkStar24");
 
-        Message.setWindowTitle("cannot open browser");
-        Message.setIcon(Message.Information);
-        Message.button(Message.Ok);
-        Message.exec();
-  }
+      Message.setWindowTitle("cannot open browser");
+      Message.setIcon(Message.Information);
+      Message.button(Message.Ok);
+      Message.exec();
+    }
 }
 
 void MainWindow::on_actionLicense_triggered()
@@ -203,42 +199,42 @@ void MainWindow::on_actionLicense_triggered()
 
 void MainWindow::on_XraiseY_clicked()
 {
-     ui->Display->setText(ui->Display->text() + "^");
+  ui->Display->setText(ui->Display->text() + "^");
 }
 
 QString MainWindow::SpecialbtClicked(QString Exp)
 {
 
-    Checker c(Exp);
-    Eval E;
-    QString ExpressionToParse ="(";
-    ExpressionToParse += Exp;
-    ExpressionToParse += ")";
-    QString Val;
+  Checker c(Exp);
+  Eval E;
+  QString ExpressionToParse ="(";
+  ExpressionToParse += Exp;
+  ExpressionToParse += ")";
+  QString Val;
 
-    Parse P(ExpressionToParse);
+  Parse P(ExpressionToParse);
 
-    if(c.HasBalancedParanthesis())
-      {
-        P.ParseFunction();
-        Val = E.FinalVal();
-        //HistoryString += Exp + " = " + Val+"\n";
-        //ui->Display->setText(Val);
-        //ui->HistoryLabel->setText(HistoryString);
-      }
-    else
-      {
-        //FIXME : Fix the geometry of the Error Box
-        QMessageBox MB;
-        MB.setText("Syntax Error");
-        MB.setIcon(MB.Warning);
-        MB.setInformativeText("Entered Input doesn't have balanced paranthesis");
-        MB.setGeometry((MainWindow::x()+MainWindow::width()/2), (MainWindow::y()+MainWindow::height()/2),
-                       (MainWindow::width()/3), (MainWindow::height()/3));
-        MB.exec();
-      }
+  if(c.HasBalancedParanthesis())
+    {
+      P.ParseFunction();
+      Val = E.FinalVal();
+      //HistoryString += Exp + " = " + Val+"\n";
+      //ui->Display->setText(Val);
+      //ui->HistoryLabel->setText(HistoryString);
+    }
+  else
+    {
+      //FIXME : Fix the geometry of the Error Box
+      QMessageBox MB;
+      MB.setText("Syntax Error");
+      MB.setIcon(MB.Warning);
+      MB.setInformativeText("Entered Input doesn't have balanced paranthesis");
+      MB.setGeometry((MainWindow::x()+MainWindow::width()/2), (MainWindow::y()+MainWindow::height()/2),
+                     (MainWindow::width()/3), (MainWindow::height()/3));
+      MB.exec();
+    }
 
-    return Val;
+  return Val;
 }
 
 void MainWindow::on_Xraise2_clicked()
@@ -273,22 +269,22 @@ void MainWindow::on_SQRT_clicked()
 
 void MainWindow::on_Pi_clicked()
 {
-    ui->Display->setText(ui->Display->text() + "3.141");
+  ui->Display->setText(ui->Display->text() + "3.141");
 }
 
 void MainWindow::on_NFact_clicked()
 {
-    qint64 Number = ui->Display->text().toLong();
-    qint64 factorial = 1;
+  qint64 Number = ui->Display->text().toLong();
+  qint64 factorial = 1;
 
-    for (qint64 i =1; i<=Number; i++)
-      {
-        factorial *= i;
-      }
+  for (qint64 i =1; i<=Number; i++)
+    {
+      factorial *= i;
+    }
 
-    ui->Display->setText(QString::number(factorial));
-    HistoryString += QString::number(Number) + "! = " + QString::number(factorial) + "\n";
-    ui->HistoryLabel->setText(HistoryString);
+  ui->Display->setText(QString::number(factorial));
+  HistoryString += QString::number(Number) + "! = " + QString::number(factorial) + "\n";
+  ui->HistoryLabel->setText(HistoryString);
 }
 
 void MainWindow::on_Log_clicked()
@@ -324,7 +320,7 @@ void MainWindow::on_btCos_clicked()
   QString Exp = ui->Display->text();
   QString Val = SpecialbtClicked(Exp);
   ui->Display->setText(QString::number(cosf64(Val.toDouble())));
-   HistoryString += "COS(" + Exp + ") = " + ui->Display->text() + "\n";
+  HistoryString += "COS(" + Exp + ") = " + ui->Display->text() + "\n";
   ui->HistoryLabel->setText(HistoryString);
 }
 
@@ -333,18 +329,42 @@ void MainWindow::on_btTan_clicked()
   QString Exp = ui->Display->text();
   QString Val = SpecialbtClicked(Exp);
   ui->Display->setText(QString::number(tanf64(Val.toDouble())));
-   HistoryString += "TAN(" + Exp + ") = " + ui->Display->text() + "\n";
+  HistoryString += "TAN(" + Exp + ") = " + ui->Display->text() + "\n";
   ui->HistoryLabel->setText(HistoryString);
 }
 
 void MainWindow::on_InverseX_clicked()
 {
-    QString Exp = ui->Display->text();
-    QString Val = SpecialbtClicked(Exp);
+  QString Exp = ui->Display->text();
+  QString Val = SpecialbtClicked(Exp);
 
-    double Result = 1/Val.toDouble();
+  double Result = 1/Val.toDouble();
 
-    ui->Display->setText(QString::number(Result));
-    HistoryString += "1/" + Exp + " = " + Val + "\n";
-    ui->HistoryLabel->setText(HistoryString);
+  ui->Display->setText(QString::number(Result));
+  HistoryString += "1/" + Exp + " = " + Val + "\n";
+  ui->HistoryLabel->setText(HistoryString);
+}
+
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+  qDebug() << event->key();
+  if(event->key() >=40 && event->key() <=57)
+    {
+      char PressedButton  = static_cast<char>(event->key());
+      ui->Display->setText(ui->Display->text() + PressedButton);
+    }
+
+  switch(event->key())
+    {
+    case 16777221:
+    case 16777220:
+      ui->btEq->animateClick();
+      break;
+
+     case 94:
+      ui->XraiseY->animateClick();
+      break;
+    }
+
 }
